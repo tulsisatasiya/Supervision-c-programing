0142014b95480ab27b81b4fa17308316b84d8a01
tulsisatasiya <115995210+tulsisatasiya@users.noreply.github.com>
2023-03-31 18:20:32 +0530

    Add files via upload

---
+++ copy.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    FILE *demo, *destination;
+    char c;
+
+    demo = fopen("demo.txt", "r");
+    destination = fopen("abcd.txt", "w");
+
+    if (demo == NULL || destination == NULL) {
+        printf("Error opening file.\n");
+        return 1;
+    }
+
+    while ((c = fgetc(demo)) != EOF) {
+        fputc(c, destination);
+    }
+
+    fclose(demo);
+    fclose(destination);
+
+    printf("File copied successfully.\n");
+
+    return 0;
+}
